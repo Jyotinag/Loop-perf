a4f1df2178a821aabe54324be7139627e61e7d2b
JYOTIRMAY NAG SETU <setu925@bb.org.bd>
2022-01-05 16:08:32 +0600

    Config File Read

--- loop-perf.cpp
+++ loop-perf.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void readConfig(){
+    std::ifstream cFile ("config.txt");
+    if(cFile.is_open())
+    {
+        std::string line;
+        while(getline(cFile, line))
+        {
+            //line.erase(std::remove_if(line.begin(),line.end(), isspace)),line.end());
+            if(line[0]=='#'|| line.empty())
+                continue;
+            auto delimiterPos = line.find("=");
+            auto name = line.substr(0,delimiterPos);
+            auto value = line.substr(delimiterPos + 1);
+            std::cout << name <<" "<< value << '\n';
+        }
+    }
+    else
+    {
+        std::cerr <<"Couldn't open config file for reading.\n";
+    }
+}
+
 int main()
 {
-    print("Hello World\n");
+    readConfig();
+    printf("Hello World\n");
     return 0;
 }
