9c34a1f964c99b05b3855a2df25428b927fef12b
JYOTIRMAY NAG SETU <setu925@bb.org.bd>
2022-01-09 11:32:02 +0600

    First Commit for Python Parser

---
+++ loop-instruction-identifier.py
@@ -0,0 +1,14 @@
+import os
+
+fileName = input()
+with open(fileName) as file:
+    file_contents = file.readline()
+    #print(file_contents)
+
+loops = []    
+for line in file_contents:
+    print(line)
+    if "for" in line:
+        loops.append(line)
+print(loops)
+
