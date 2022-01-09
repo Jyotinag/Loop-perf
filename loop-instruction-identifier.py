1bc0cebb16cebc1d10d4eb2fecef5c8465a37823
Jyotinag <jyotinag.csedu@gmail.com>
2022-01-09 22:25:20 +0600

    first version of the standard perforation

--- loop-instruction-identifier.py
+++ loop-instruction-identifier.py
@@ -2,6 +2,7 @@ import os
 import re
 import pandas as pd
 import errno
+import subprocess
 
 #reading the c/c++ file
 #returns a string 
@@ -11,7 +12,7 @@ def read_c_file():
         with open(fileName) as file:
             file_contents = file.read()
             #print(file_contents)
-        return file_contents
+        return file_contents, fileName
     except:
         raise FileNotFoundError(
             errno.ENOENT, os.strerror(errno.ENOENT), fileName
@@ -49,15 +50,39 @@ def make_perforated_file(indexList, contentList, loopList):
     print(contentList)
     return contentList
 
+def analysis_perf(perfFileName, fileName):
+    subprocess.check_call(
+    ('gcc', perfFileName),
+    stdin=subprocess.DEVNULL)
+
+    with open('input.txt') as infile, open('outputperf.txt', 'w') as outfile:
+        subprocess.check_call(
+            ('./a.out',),
+            stdin=infile,
+            stdout=outfile,
+            universal_newlines=True)
+    
+    subprocess.check_call(
+    ('gcc', fileName),
+    stdin=subprocess.DEVNULL)
+
+    with open('input.txt') as infile, open('output.txt', 'w') as outfile:
+        subprocess.check_call(
+            ('./a.out',),
+            stdin=infile,
+            stdout=outfile,
+            universal_newlines=True)
+
 
 #the main function
 def main():
-    file_contents = read_c_file()
+    file_contents, fileName = read_c_file()
     content_list = convert_string(file_contents)
     # print(content_list)
     indexList, loopList = id_loops(content_list)
     perforarte_loop(loopList)
     perf = make_perforated_file(indexList,content_list,loopList)
+    analysis_perf("perf.cpp", fileName)
     print(loopList)
     print(indexList)
     print(type(file_contents))
